8f5897f0a8eb9df96d8b1328dc9a479da8e1794f
William W Wold <wilcoze@gmail.com>
2017-10-10 14:45:51 -0700

    built system for translating into morse

--- app/src/main/java/net/widap/morsealert/MorseBuzzer.java
+++ app/src/main/java/net/widap/morsealert/MorseBuzzer.java
@@ -4,28 +4,93 @@ import android.app.Service;
 import android.content.Context;
 import android.os.Vibrator;
 import android.os.VibrationEffect;
+import android.provider.Settings;
 
-public class MorseBuzzer {
-    Vibrator vibrator;
-    String text;
-    public MorseBuzzer(String text, Service service) {
+/*
+CODE:
+. : dot
+- : dash
+  (space): space between letters
+/ : space between words
+| : invalid character or other error
+ */
+
+class MorseBuzzer {
+    private Vibrator vibrator;
+    private String text;
+
+    private int dotTime = 200;
+    private int dashTime = dotTime * 3;
+    private int smallSpaceTime = dotTime;
+    private int letterSpaceTime = dashTime;
+    private int wordSpaceTime = dotTime * 7;
+    private int extraLongTime = 1000;
+
+    MorseBuzzer(String text, Service service) {
         this.text = text;
         vibrator = (Vibrator) service.getSystemService(Context.VIBRATOR_SERVICE);
     }
 
-    void buzzLetter(char c) {
-        vibrator.vibrate(1000);
+    private void sleepFor(int time) {
+        try {
+            Thread.sleep(time);
+        } catch (Exception e) {
+            // Whats a thread gotta do to get some sleep?
+        }
+    }
+
+    private void buzzFor(int time) {
+        vibrator.vibrate(dotTime);
+        sleepFor(time);
+    }
+
+    private String getCodeFor(char c) {
+        switch (c) {
+            case 'a': return ".-";
+            case 'b': return "-...";
+            case 'c': return "-.-.";
+            case 'd': return "-..";
+            case ' ': return "/";
+            default: return "|";
+        }
+    }
+
+    private String getCodeFor(String text) {
+        text = text.toLowerCase();
+        String out = "";
+        for (int i = 0; i < text.length(); i++) {
+            if (i > 0) {
+                out += " ";
+            }
+            char c = text.charAt(i);
+            out += getCodeFor(c);
+        }
+        return out;
+    }
+
+    private void buzzCode(char c) {
+        switch (c) {
+            case '.': buzzFor(dotTime);
+            case '-': buzzFor(dashTime);
+            case '|': buzzFor(extraLongTime);
+            default: vibrator.vibrate(extraLongTime);
+        }
+    }
+
+    private void buzzCode(String code) {
+        for (int i = 0; i < text.length(); i++){
+            if (i != 0) {
+                sleepFor(smallSpaceTime);
+            }
+            char c = text.charAt(i);
+            buzzCode(c);
+        }
     }
 
     void go() {
-        for (int i = 0; i < text.length(); i++){
-            char c = text.charAt(i);
-            buzzLetter(c);
-            try {
-                Thread.sleep(400);
-            } catch (Exception e) {
-                // Whats a thread gotta do to get some sleep?
-            }
-        }
+        System.out.println("buzzing text: " + text);
+        String code = getCodeFor(text);
+        System.out.println("buzzing code: " + code);
+        buzzCode(code);
     }
 }
