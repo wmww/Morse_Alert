91af0c4799bdc1186016a5e385d7c7cd9d19a4a1
William W Wold <wilcoze@gmail.com>
2017-10-10 12:00:02 -0700

    it sort of works I think

--- app/src/main/java/net/widap/morsealert/MainSettings.java
+++ app/src/main/java/net/widap/morsealert/MainSettings.java
@@ -1,13 +1,39 @@
 package net.widap.morsealert;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainSettings extends AppCompatActivity {
+    //private NotificationReceiver receiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        // startActivity(new Intent(Settings.ACTION_NOTIFICATION_LISTENER_SETTINGS));
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_settings);
+        //receiver = new NotificationReceiver();
+        //IntentFilter filter = new IntentFilter();
+        //filter.addAction(getApplicationContext().getPackageName() + ".NOTIFICATION_LISTENER_EXAMPLE");
+        //registerReceiver(receiver, filter);
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //unregisterReceiver(receiver);
+    }
+
+    /*
+    class NotificationReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            System.out.println("notification: " + intent.getStringExtra("notification_data"));
+        }
+    }
+    */
 }
---
+++ app/src/main/java/net/widap/morsealert/NotificationService.java
@@ -0,0 +1,73 @@
+package net.widap.morsealert;
+
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.provider.Settings;
+import android.service.notification.NotificationListenerService;
+import android.service.notification.StatusBarNotification;
+
+public class NotificationService extends NotificationListenerService {
+    //private NotificationReceiver reciever;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        System.out.println("NotificationService created");
+        //reciever = new NotificationReceiver();
+        //IntentFilter filter = new IntentFilter();
+        //filter.addAction(getApplicationContext().getPackageName() + ".NOTIFICATION_LISTENER_SERVICE_EXAMPLE");
+        //registerReceiver(reciever, filter);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        //unregisterReceiver(reciever);
+    }
+
+    @Override
+    public void onNotificationPosted(StatusBarNotification data) {
+        System.out.println("notification text: " + data.getNotification().tickerText);
+        // data.getId()
+        // data.getNotification().tickerText
+        // data.getPackageName());
+        // Intent intent = new  Intent(getApplicationContext().getPackageName() + ".NOTIFICATION_LISTENER_EXAMPLE");
+        // intent.putExtra("notification_event","notification from " + data.getPackageName() + "\n");
+        // sendBroadcast(intent);
+    }
+
+    @Override
+    public void onNotificationRemoved(StatusBarNotification data) {
+        //Intent intent = new  Intent(getApplicationContext().getPackageName() + ".NOTIFICATION_LISTENER_EXAMPLE");
+        //intent.putExtra("notification_event","notification removed " + data.getPackageName() + "\n");
+        //sendBroadcast(intent);
+    }
+
+    /*
+    class NotificationReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if(intent.getStringExtra("command").equals("clearall")){
+                NotificationService.this.cancelAllNotifications();
+            }
+            else if(intent.getStringExtra("command").equals("list")){
+                Intent i1 = new  Intent(getApplicationContext().getPackageName() + ".NOTIFICATION_LISTENER_EXAMPLE");
+                i1.putExtra("notification_event","=====================");
+                sendBroadcast(i1);
+                int i=1;
+                for (StatusBarNotification notification : NotificationService.this.getActiveNotifications()) {
+                    Intent intent2 = new  Intent(getApplicationContext().getPackageName() +".NOTIFICATION_LISTENER_EXAMPLE");
+                    intent2.putExtra("notification_event",i +" " + notification.getPackageName() + "\n");
+                    sendBroadcast(intent2);
+                    i++;
+                }
+                Intent i3 = new  Intent("com.kpbird.nlsexample.NOTIFICATION_LISTENER_EXAMPLE");
+                i3.putExtra("notification_event","===== Notification List ====");
+                sendBroadcast(i3);
+
+            }
+
+        }
+    }
+    */
+}
