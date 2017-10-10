50213c1273f22905d95b83707c083f45dc14cd1b
William W Wold <wilcoze@gmail.com>
2017-10-09 18:59:33 -0700

    initial commit

---
+++ app/src/main/java/net/widap/morsealert/MainSettings.java
@@ -0,0 +1,13 @@
+package net.widap.morsealert;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainSettings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_settings);
+    }
+}
